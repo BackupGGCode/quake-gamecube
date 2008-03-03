ba2ce8b417df912148487f2d714575207b07f689
mackay.pete <mackay.pete@185e6315-e336-0410-98b0-3f65cb188f0e>
2008-03-03 18:10:12 +0000

    Added some missing SDL init flags. Changed res to 320x240.

--- trunk/quake/src/sdl/main.cpp
+++ trunk/quake/src/sdl/main.cpp
@@ -52,7 +52,7 @@ qboolean isDedicated = qfalse;
 int main(int argc, char* argv[])
 {
 	// Initialise.
-	if (SDL_Init(SDL_INIT_VIDEO /*| SDL_INIT_AUDIO | SDL_INIT_JOYSTICK | SDL_INIT_TIMER | SDL_INIT_NOPARACHUTE*/) < 0)
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK /*| SDL_INIT_TIMER | SDL_INIT_NOPARACHUTE*/) < 0)
 	{
 		printf("SDL_Init failed (%s)\n", SDL_GetError());
 		return EXIT_FAILURE;
