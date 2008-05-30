72ef72757c38a719136287a58110443e28ce3040
eluancm <eluancm@185e6315-e336-0410-98b0-3f65cb188f0e>
2008-05-30 05:35:29 +0000

    Broken keyboard...

--- quake-wii/src/wii/input_wiimote.h
+++ quake-wii/src/wii/input_wiimote.h
@@ -1,8 +1,8 @@
 #ifndef __WIIMOTE_INPUT_H__
 #define __WIIMOTE_INPUT_H__
 
-/* TODO: aspect ratio?*/
+/* TODO: aspect ratio? */
 #define WIIMOTE_IR_RES_X 560
 #define WIIMOTE_IR_RES_Y 420
 
-#endif /* __WIMOTE_INPU_H__ */
+#endif  /* __WIMOTE_INPUT_H__ */
