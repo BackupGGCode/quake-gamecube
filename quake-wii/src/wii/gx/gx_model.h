6ba4f0891eb91e6341ef3d238e1ea19c48eff196
eluancm <eluancm@185e6315-e336-0410-98b0-3f65cb188f0e>
2008-08-29 03:49:43 +0000

    removed all (or nearly all) the references to serial/ipx networking

--- quake-wii/src/wii/gx/gx_model.h
+++ quake-wii/src/wii/gx/gx_model.h
@@ -21,8 +21,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef __MODEL__
 #define __MODEL__
 
-#include <ogc/gx.h>
-
 #include "../../generic/modelgen.h"
 #include "../../generic/spritegn.h"
 
