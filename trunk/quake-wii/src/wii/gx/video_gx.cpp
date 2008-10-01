032140ac458a6186e7c118ac603989ac7245f263
eluancm <eluancm@185e6315-e336-0410-98b0-3f65cb188f0e>
2008-10-01 19:30:19 +0000

    update gpl headers...

--- trunk/quake-wii/src/wii/gx/video_gx.cpp
+++ trunk/quake-wii/src/wii/gx/video_gx.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 1996-1997 Id Software, Inc.
+Copyright (C) 2008 Eluan Miranda
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
--- trunk/quake-wii/src/wii/input.cpp
+++ trunk/quake-wii/src/wii/input.cpp
@@ -1,6 +1,7 @@
 /*
 Quake GameCube port.
 Copyright (C) 2007 Peter Mackay
+Copyright (C) 2008 Eluan Miranda
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
